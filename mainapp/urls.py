b7b1e5c72ad0be3d79eaeb63ba59aeb0418c3c07
siberianshark <siberiansharkgithub.com>
2022-11-03 02:56:12 -0700

    lesson3

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,15 +1,15 @@
-from django.urls import path
-
 from mainapp import views
+from django.urls import path
 from mainapp.apps import MainappConfig
 
 app_name = MainappConfig.name
 
 urlpatterns = [
-    path('', views.IndexView.as_view()),
-    path('', views.NewsPageView.as_view()),
-    path('', views.CoursesPageView.as_view()),
-    path('', views.ContactsPageView.as_view()),
-    path('', views.DocSitePageView.as_view()),
-    path('', views.LoginPageView.as_view()),
+    path('contacts/', views.ContactsView.as_view(), name="contacts"),
+    path('courses/', views.CoursesView.as_view(), name="courses"),
+    path('docs/', views.DocsView.as_view(), name="docs"),
+    path('', views.IndexView.as_view(), name="mainapp"),
+    path('login/', views.LoginView.as_view(), name="login"),
+    path('news/', views.NewsView.as_view(), name="news"),
+    path('news/<int:page>/', views.NewsWithPaginatorView.as_view(), name="news_paginator"),
 ]
